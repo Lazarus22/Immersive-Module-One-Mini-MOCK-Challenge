4db45042183ebec6a8cd8984fe980660de47431f
Lazarus22 <66386687+Lazarus22@users.noreply.github.com>
2020-07-20 13:40:31 -0400

    Add files via upload

--- console.rb
+++ console.rb
@@ -8,7 +8,9 @@ roger_zelazny = Author.new ('roger_zelazny')
 
 steven_king.write_book('the_stand', 500000)
 steven_king.write_book('the_dark_tower', 66000)
+jeff_vandermeer.write_book('annihilation', 55000)
 roger_zelazny.write_book('nine_princes_in_amber', 56000)
+
 
 binding.pry
 "@"
